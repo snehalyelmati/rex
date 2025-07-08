697dd95b4010b00523d2ccc07397a860a5f454bc
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2025-07-09 03:09:18 +0530

    Planner agent MVP 1 with local memory

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 import asyncio
 import logging
-from typing import Annotated, Dict, Sequence, TypedDict
 
 import streamlit as st
-from langchain_core.messages import (
-    AIMessage,
-    BaseMessage,
-    HumanMessage,
-    SystemMessage,
-    ToolMessage,
-)
-from langchain_core.tools import BaseTool, tool
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
-from langchain_openai import ChatOpenAI
-from langgraph import graph
-from langgraph.graph import END, START, StateGraph
-from langgraph.graph.message import add_messages
-from langgraph.prebuilt import ToolNode
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import StdioServerParameters, stdio_client
+from PIL.Image import Image
 
-from src.mcp_client.client import MCPOpenAIClient
+from src.agent.planner_agent import AgentState, build_agent
 
 logging.basicConfig(
     level=logging.INFO,
@@ -28,50 +16,14 @@ logging.basicConfig(
     datefmt="%m/%d/%y %H:%M:%S",
 )
 
+# st.set_page_config(layout="wide")
+
 # MCP Server parameters
 server_params = StdioServerParameters(
     command="python",
     args=["src/mcp_servers/git_mcp_server.py"],
     env=None,
 )
-
-
-class AgentState(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], add_messages]
-    tools: Dict[str, BaseTool]
-
-
-def process(state: AgentState) -> AgentState:
-    system_prompt = SystemMessage(
-        content="You are a helpful, honest and harmless assistant, do your best to answer the user's query. Depend primarily on the tools available to you."
-    )
-
-    response = llm.invoke([system_prompt] + state["messages"])
-    return {"messages": [response]}
-
-
-async def custom_tool_node(state: AgentState):
-    """Performs the tool call"""
-
-    result = []
-    for tool_call in state["messages"][-1].tool_calls:
-        tool = state["tools"][tool_call["name"]]
-        observation = await tool.ainvoke(tool_call["args"])
-        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
-        result.append(
-            AIMessage(
-                content=f"Calling tool: `{tool.name}` with Args: `{tool_call['args']}`"
-            )
-        )
-    return {"messages": result}
-
-
-def should_continue(state: AgentState):
-    last_message = state["messages"][-1]
-    if not last_message.tool_calls:
-        return "end"
-    else:
-        return "continue"
 
 
 def display_chat_history():
@@ -97,35 +49,20 @@ async def main():
 
             if "messages" not in st.session_state:
                 st.session_state.messages = []
+                st.session_state.messages.append(
+                    AIMessage(content="Hi there, how can I help?")
+                )
 
             display_chat_history()
 
             # Prerequisites for Agents
-            tools = await load_mcp_tools(session)
-            tools_by_name = {tool.name: tool for tool in tools}
+            tools_available = await load_mcp_tools(session)
+            tools_by_name = {tool.name: tool for tool in tools_available}
             # st.write("Available tools:", [tool for tool in tools][0])
 
-            global llm
-            llm = ChatOpenAI(model="gpt-4o", temperature=0).bind_tools(tools)
-
-            # Building Graph
-            graph = StateGraph(AgentState)
-
-            graph.add_node("process_node", process)
-
-            # tool_node = ToolNode(tools=tools)
-            graph.add_node("tools", custom_tool_node)
-
-            graph.add_edge(START, "process_node")
-            graph.add_conditional_edges(
-                source="process_node",
-                path=should_continue,
-                path_map={"continue": "tools", "end": END},
-            )
-            graph.add_edge("tools", "process_node")
-            graph.add_edge("process_node", END)
-
-            agent = graph.compile()
+            agent = await build_agent(tools_available)
+            with st.sidebar:
+                st.image(agent.get_graph().draw_mermaid_png())
 
             # Implementing Agentic workflow
             if prompt := st.chat_input("How can I help?"):
@@ -133,10 +70,17 @@ async def main():
                 st.session_state.messages.append(HumanMessage(content=prompt))
 
                 with st.spinner("Thinking..."):
-                    state = AgentState(
-                        messages=st.session_state.messages, tools=tools_by_name
+                    # state = AgentState(
+                    #     messages=st.session_state.messages, tools=tools_by_name
+                    # )
+                    # st.session_state.messages = (await agent.ainvoke(state))["messages"]
+                    response = await agent.ainvoke(
+                        AgentState(input=prompt, messages=st.session_state.messages)
                     )
-                    st.session_state.messages = (await agent.ainvoke(state))["messages"]
+                    st.write(response)
+                    st.session_state.messages.append(
+                        AIMessage(response["past_steps"][-1][-1])
+                    )
 
                 # Display tool message along with AIMessage
                 if len(st.session_state.messages) > 2:
---
+++ src/agent/planner_agent.py
@@ -0,0 +1,138 @@
+import operator
+from typing import Annotated, Dict, List, Tuple, Union
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import BaseTool
+from langchain_openai import ChatOpenAI
+from langgraph.graph import END, START, StateGraph
+from langgraph.prebuilt import create_react_agent
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
+
+
+class AgentState(TypedDict):
+    input: str
+    plan: List[str]
+    past_steps: Annotated[List[Tuple], operator.add]
+    response: str
+
+
+class Plan(BaseModel):
+    """Plan to follow in future"""
+
+    steps: List[str] = Field(
+        description="different steps to follow, should be in sorted order"
+    )
+
+
+class Response(BaseModel):
+    """Response to user."""
+
+    response: str
+
+
+class Act(BaseModel):
+    """Action to perform."""
+
+    action: Union[Response, Plan] = Field(
+        description="Action to perform. If you want to respond to user, use Response. "
+        "If you need to further use tools to get the answer, use Plan."
+    )
+
+
+planner_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """For the given objective, come up with a simple step by step plan. \
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.""",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+)
+planner = planner_prompt | ChatOpenAI(
+    model="gpt-4.1-2025-04-14", temperature=0
+).with_structured_output(Plan)
+
+replanner_prompt = ChatPromptTemplate.from_template(
+    """For the given objective, come up with a simple step by step plan. \
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
+
+Your objective was this:
+{input}
+
+Your original plan was this:
+{plan}
+
+You have currently done the follow steps:
+{past_steps}
+
+Update your plan accordingly. If no more steps are needed and you can return to the user, then respond with that. Otherwise, fill out the plan. Only add steps to the plan that still NEED to be done. Do not return previously done steps as part of the plan."""
+)
+
+replanner = replanner_prompt | ChatOpenAI(
+    # model="gpt-4o", temperature=0
+    model="gpt-4.1-2025-04-14",
+    temperature=0,
+).with_structured_output(Act)
+
+
+async def execute_step(state: AgentState):
+    plan = state["plan"]
+    plan_str = "\n".join(f"{i+1}. {step}" for i, step in enumerate(plan))
+    task = plan[0]
+    task_formatted = f"""For the following plan:
+{plan_str}\n\nYou are tasked with executing step {1}, {task}."""
+    agent_response = await agent_executor.ainvoke(
+        {"messages": [("user", task_formatted)]}
+    )
+    return {
+        "past_steps": [(task, agent_response["messages"][-1].content)],
+    }
+
+
+async def plan_step(state: AgentState):
+    plan = await planner.ainvoke({"messages": [("user", state["input"])]})
+    return {"plan": plan.steps}
+
+
+async def replan_step(state: AgentState):
+    output = await replanner.ainvoke(state)
+    if isinstance(output.action, Response):
+        return {"response": output.action.response}
+    else:
+        return {"plan": output.action.steps}
+
+
+def should_end(state: AgentState):
+    if "response" in state and state["response"]:
+        return END
+    else:
+        return "agent"
+
+
+async def build_agent(tools_available):
+    llm = ChatOpenAI(model="gpt-4o")
+    prompt = "You are a helpful assistant."
+
+    global agent_executor
+    agent_executor = create_react_agent(llm, tools_available, prompt=prompt)
+
+    builder = StateGraph(AgentState)
+
+    builder.add_node("planner", plan_step)
+    builder.add_node("agent", execute_step)
+    builder.add_node("replan", replan_step)
+
+    builder.add_edge(START, "planner")
+    builder.add_edge("planner", "agent")
+    builder.add_edge("agent", "replan")
+    builder.add_conditional_edges(
+        "replan",
+        should_end,
+        ["agent", END],
+    )
+
+    return builder.compile()
